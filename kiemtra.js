da3ed36d4b06ce5b39c6b94082dfe02f3230ab55
txLLan1710 <lanb2014929@student.ctu.edu.vn>
2023-03-30 00:49:08 +0700

    Tối 30/3

--- kiemtra.js
+++ kiemtra.js
@@ -1,4 +1,72 @@
+function validateEmail() {
+    var email = document.getElementById('inputEmail_login').value;
+    var atposition = email.indexOf("@");
+    var dotposition = email.lastIndexOf(".");
+    var messEmail = document.getElementById('errorEmail_Login');
+    if(atposition < 1 || dotposition < (atposition + 2)
+            || (dotposition + 2) >= email.length) {
+            messEmail.innerHTML = 'Vui lòng nhập email';
+        return false;
+    }
+}
 
+function validatePass() {
+    var pass = document.getElementById('inputPass_Login').value;
+    var mess = document.getElementById('errorPass_Login');
+    if(pass == ""){
+        mess.innerHTML = 'Pass không được để trống';
+        return false;
+    }
+    if(pass.length < 6 || pass.length > 15){
+        mess.innerHTML = 'Vui long nhập mật khẩu';
+        return false;
+    }else{
+        mess.innerHTML = '';
+    }
+}
+function validation_Login(){
+    validateEmail();
+    validatePass();
+}
+
+function validateName() {
+
+}
+
+function validateEmail_Register() {
+
+}
+function validatePhone() {
+
+}
+function validatePass_Register() {
+
+}
+function validate_Register() {
+    validateName();
+    validateEmail_Register();
+    validatePhone();
+    validatePass_Register();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 function kiemtra() {
     var e = document.getElementById('email').value;
     var p = document.getElementById('pass').value;
@@ -6,7 +74,7 @@ function kiemtra() {
         document.getElementById('displayemail').innerHTML = '<i>Vui lòng nhập email</i>';
         return false;
     }
-    if( p == ""){
+    if( p.length < 6 ){
         document.getElementById('checkpass').innerHTML = 'Vui lòng nhập password';
         return false;
     }
@@ -23,6 +91,8 @@ function checkEmail() {
         alert("Nhập đúng định dạng") 
     }
 }
+*/
+
 
 
 
